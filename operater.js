c447f2acc74744813f8146c5b26523d4436fb21d
amitr25 <14054amitraj@gmail.com>
2024-08-30 22:40:52 +0530

    hlo

---
+++ operater.js
@@ -0,0 +1,16 @@
+console.log("operater in js")
+let a = 10; 
+let b = 4;
+console.log("a+b=", a+b)
+console.log("a-b=", a-b)
+console.log("a/b=", a/b)
+console.log("a*b=", a*b)
+console.log("a**b=", a**b)
+
+//increment and decreement
+console.log("++a=",++a)
+console.log("a++=",a++)
+console.log("--a=",--a)
+console.log("a--=",a--)
+console.log("a=",a)
+console.log("a--=",a--)
